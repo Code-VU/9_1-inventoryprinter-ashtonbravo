11b4f359fb7cf51ba78922f0b5c48b8969394adb
Ashton Bravo <Ashton.Bravo@vu.com>
2022-05-09 22:29:12 -0500

    test

--- displayInventory.py
+++ displayInventory.py
@@ -2,6 +2,11 @@ stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'ring': 1, 'apple': 12}
 
 def displayInventory(inventory):
     # your code goes here
-    stuff = {'rope':1, 'torch': 6, 'gold coin': 42, 'ring': 1, 'apple': 12} 
-    print('Inventory: ', stuff)
+    stuff = {'rope':1, 'torch': 6, 'gold coin': 42, 'ring': 1, 'apple': 12}
+    for key in stuff:
+        print(key, ':', stuff[key])
+#displayInventory(stuff)
+    
+print('inventory: ')
 displayInventory(stuff)
+print('total number of items:', 62) 
