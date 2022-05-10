4e319be993ddff57c4fa4fedfd9c4c2d4c2bd6ba
Ashton Bravo <Ashton.Bravo@vu.com>
2022-05-09 22:04:52 -0500

    test

--- displayInventory.py
+++ displayInventory.py
@@ -2,5 +2,6 @@ stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'ring': 1, 'apple': 12}
 
 def displayInventory(inventory):
     # your code goes here
-
-##displayInventory(stuff)
+    stuff = {'rope':1, 'torch': 6, 'gold coin': 42, 'ring': 1, 'apple': 12} 
+    print('Inventory: ', stuff)
+displayInventory(stuff)
